339e2f8917300fdcc9dd292f8b7eb5875544db9e
AGVVsivakiran <102651788+AGVVsivakiran@users.noreply.github.com>
2025-02-16 17:27:16 +0530

    Update popup.ts
    Pop-up, close, click outside popup contain, and esc button

--- POP-Up/popup.ts
+++ POP-Up/popup.ts
@@ -8,3 +8,12 @@ openpopup(){
 closepopup(){
 this.show=false;
 }
+// if we write this code  ( this.show=false; ) it will close when we click on any where in the overlay(class) box
+}
+// Listen for Escape key press and close the popup
+@HostListener('document:keydown.escape')
+closeOnEscape() {
+  this.show = false;
+  //here add import { HostListener } from '@angular/core';
+
+}
